d7d6892a9aaf66c49e2b3aa87752baffb4091b69
Nathan Taing <ntaing24@students.polytechnic.org>
2024-01-11 09:28:49 -0800

    Added Scanner, A, B, and C

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,6 +1,14 @@
+import java.util.*;
 public class QuadraticSolver {
 public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
     System.out.println("Welcome to the Quadratic Solver");
-    System.out.println("Enter an A, B, and C");
+    System.out.println("Enter the first side");
+    int A = input.nextInt();
+    System.out.println("Enter the second side");
+    int B = input.nextInt();
+    System.out.println("Enter the third side");
+    int C = input.nextInt();
+    
 }
 }
