e9742834b6be623d118209ee674a57367514ca40
Nathan Taing <ntaing24@students.polytechnic.org>
2024-01-11 09:21:26 -0800

    Added enter message

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -1,6 +1,6 @@
 public class QuadraticSolver {
 public static void main(String[] args) {
     System.out.println("Welcome to the Quadratic Solver");
-    
+    System.out.println("Enter an A, B, and C");
 }
 }
